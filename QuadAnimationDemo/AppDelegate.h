f0502f86e8eb379a03194a079ebae57392b79d64
billwang1990 <billwang1990@gmail.com>
2015-07-03 14:44:52 +0800

    Initial Commit

---
+++ QuadAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuadAnimationDemo
+//
+//  Created by wangyaqing on 15/7/3.
+//  Copyright (c) 2015年 billwang1990.github.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuadAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuadAnimationDemo
+//
+//  Created by wangyaqing on 15/7/3.
+//  Copyright (c) 2015年 billwang1990.github.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
